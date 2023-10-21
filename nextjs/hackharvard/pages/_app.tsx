179cadb91d36cc4716b75136dc2916c48d348e32
kylieb12 <119005055+kylieb12@users.noreply.github.com>
2023-10-21 03:08:01 -0400

    home page

--- nextjs/hackharvard/pages/_app.tsx
+++ nextjs/hackharvard/pages/_app.tsx
@@ -1,4 +1,3 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
 
--- nextjs/hackharvard/pages/_document.tsx
+++ nextjs/hackharvard/pages/_document.tsx
@@ -4,10 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         </Head>
       <body>
+  
 
         <Main />
         <NextScript />
